c2d336bac509e74de01321e17173a6633da39de7
Khoa Nguyen <nguyenkhoa@gmail.com>
2018-04-14 19:22:24 -0700

    change to mongo setup
    change to mongo setup

--- config/index.js
+++ config/index.js
@@ -1,6 +1,7 @@
 module.exports = {  
     // Database connection information
-    'database': 'mongodb://localhost/pacificboatclub',
+    //'database': 'mongodb://localhost/pacificboatclub',
+    'database': 'mongodb://isco:123@ds125479.mlab.com:25479/pacificboatclub',
 
     // Setting port for server
     'port': process.env.PORT || 3000,  
